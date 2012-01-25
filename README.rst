08d37fa1a3d55d2001a9077d73897a6523a29aa3
Rodrigo Manhães <rmanhaes@gmail.com>
2012-01-25 16:30:16 -0200

    open house

---
+++ README.rst
@@ -0,0 +1,5 @@
+Digital Library 2.0
+===================
+
+Coming soon...
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DigitalLibrary::Application.config.secret_token = 'a55c9214897f762a5e9b34d1946f74a06e728fb230c966460a27f145c77b1fe8b501544a9a85d462339a3181cb99e8f489d959fa6cb356be32773bb690b267bb'
