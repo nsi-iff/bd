3f6c2e1e7d91ab1a11716b7a4529c293b8699b84
Natanael de Araújo <internael@gmail.com>
2012-02-08 02:59:55 -0200

    criado método para facilitar a validação dos campos obrigatórios do conteúdo básico

--- spec/support/conteudo_basico.rb
+++ spec/support/conteudo_basico.rb
@@ -2,7 +2,6 @@
 
 def submeter_conteudo(tipo, opcoes = {})
   visit send(:"new_#{tipo}_path")
-  fill_in 'Link', with: opcoes[:link] || 'http://nsi.iff.edu.br'
   fill_in 'Arquivo', with: opcoes[:arquivo] || ''
   fill_in 'Título',
     with: opcoes[:titulo] || 'A Proposal for Ruby Performance Improvements'
@@ -21,4 +20,17 @@ def submeter_conteudo(tipo, opcoes = {})
     with: opcoes[:campus] || 'Campos Centro'
   yield if block_given?
   click_button 'Salvar'
+end
+
+def validar_conteudo(opcoes = {})
+  page.should have_content opcoes[:titulo] || 'Título: A Proposal for Ruby Performance Improvements'
+  (page.should have_content opcoes[:link] || 'http://www.rubyconf.org/articles/1') unless opcoes[:arquivo]
+  page.should have_content opcoes[:arquivo] || ''
+  page.should have_content opcoes[:grande_area_de_conhecimento] || 'Grande Área de Conhecimento: Ciência da Computação'
+  page.should have_content opcoes[:area_de_conhecimento] || 'Área de Conhecimento: Linguagens de Programação'
+  unless opcoes[:autores] == false
+    page.should have_content opcoes[:nome_autor] || 'Autor: Yukihiro Matsumoto'
+    page.should have_content opcoes[:lattes_autor] || 'Curriculum Lattes: http://lattes.cnpq.br/1234567890'
+  end
+  page.should have_content opcoes[:campus] || 'Campus: Campos Centro'
 end
