b38806214d26ebc6a4bf7b0d7dc3401ed5fbf05d
Pablo Xavier <pabloxavierbs@gmail>
2012-02-08 17:51:18 -0200

    validações da local, ano, paginas (#10)

--- app/models/relatorio.rb
+++ app/models/relatorio.rb
@@ -1,5 +1,11 @@
 # encoding: utf-8
 class Relatorio < Conteudo
-  validates_numericality_of :ano_publicacao,
-                            :less_than_or_equal_to => Time.now.year
+  validates :ano_publicacao, :numero_paginas, numericality: true
+
+  validates :ano_publicacao, less_than_or_equal_to => Time.now.year
+
+  validates :local_publicacao, :ano_publicacao, :numero_paginas,
+            allow_blank: true
+
+  validates :numero_paginas, greater_than => 0
 end
