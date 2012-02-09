a0f5d3a7cc0c6f9f9927a1242a2b0e96a6e68bdf
João Felipe <jfelipe.roque@gmail.com>
2012-02-09 15:20:54 -0200

    alterada validação de ano do primeiro e último volume (#9)

--- app/models/periodico_tecnico_cientifico.rb
+++ app/models/periodico_tecnico_cientifico.rb
@@ -1,10 +1,11 @@
 # encoding: utf-8
 
 class PeriodicoTecnicoCientifico < Conteudo
+  this_year = Date.today.year
   validates :ano_primeiro_volume, 
-    numericality: { greater_than: 0, less_than_or_equal_to: Time.now.year,
+    numericality: { greater_than: 0, less_than_or_equal_to: this_year,
                     allow_blank: true }
   validates :ano_ultimo_volume,
-    numericality: { greater_than: 0, less_than_or_equal_to: Time.now.year,
+    numericality: { greater_than: 0, less_than_or_equal_to: this_year,
                     allow_blank: true }
 end
