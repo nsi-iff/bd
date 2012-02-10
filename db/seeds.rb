a10f3abd588985c6f3afce6ed440869634922a5a
Rodrigo Manhães <rmanhaes@gmail.com>
2012-02-10 17:27:54 -0200

    adicionado seed para os eixos temáticos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
+# encoding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+['Artes', 'Ciências', 'Educação', 'Eletrotécnica', 'Eletrônica', 'Filosofia',
+ 'Física', 'Gestão Ambiental', 'História', 'Informática Educativa',
+ 'Língua Inglesa', 'Língua Portuguesa', 'Matemática', 'Matemática Básica',
+ 'Psicologia', 'Química', 'Refrigeração e Climatização',
+ 'Tecnologia em Informática', 'Telecomunicações', 'Termologia',
+ 'Todas as Áreas', 'Turismo'].each do |nome_eixo|
+  EixoTematico.create! :nome => nome_eixo
+end
