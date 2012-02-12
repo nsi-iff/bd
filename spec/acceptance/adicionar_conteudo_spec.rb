9147bae27a788a0ebf5353537a53687711a76372
Rodrigo Manhães <rmanhaes@gmail.com>
2012-02-12 14:26:24 -0200

    teste de aceitacao de area e sub-area movido para feature de adicionar conteudo basico (#15)

--- spec/acceptance/adicionar_conteudo_spec.rb
+++ spec/acceptance/adicionar_conteudo_spec.rb
@@ -8,7 +8,7 @@ feature 'adicionar conteudo (referente aos dados básicos)' do
     visit new_artigo_de_evento_path
     fill_in 'Título', with: 'A Proposal for Ruby Performance Improvements'
     fill_in 'Link', with: 'http://www.rubyconf.org/articles/1'
-    select('Ciências Exatas e da Terra', :from => 'Grande Área de Conhecimento') 
+    select('Ciências Exatas e da Terra', :from => 'Grande Área de Conhecimento')
     select('Ciência da Computação', :from => 'Área de Conhecimento*')
     click_link 'Adicionar autor'
     fill_in 'Autor', with: 'Yukihiro Matsumoto'
@@ -79,5 +79,37 @@ feature 'adicionar conteudo (referente aos dados básicos)' do
       end
     end
     within('#autores') { page.should have_content "não pode ficar em branco" }
+  end
+
+  scenario "conteudo da área deve mudar quando grande área é selecionada", :javascript => true do
+    popular_area_sub_area
+    visit new_artigo_de_evento_path
+
+    select('Ciências Exatas e da Terra', from: 'Grande Área de Conhecimento')
+    select('Ciência da Computação', from: 'Área de Conhecimento*')
+
+    select('Engenharias', from: 'Grande Área de Conhecimento')
+    select('Engenharia de Produção', from: 'Área de Conhecimento*')
+
+    select('Ciências Biológicas', from: 'Grande Área de Conhecimento')
+    select('Biologia Geral', from: 'Área de Conhecimento*')
+
+    select('Ciências da Saúde', from: 'Grande Área de Conhecimento')
+    select('Enfermagem', from: 'Área de Conhecimento*')
+
+    select('Ciências Agrárias', from: 'Grande Área de Conhecimento')
+    select('Agronomia', from: 'Área de Conhecimento*')
+
+    select('Ciências Sociais Aplicadas', from: 'Grande Área de Conhecimento')
+    select('Administração', from: 'Área de Conhecimento*')
+
+    select('Ciências Humanas', from: 'Grande Área de Conhecimento')
+    select('Teologia', from: 'Área de Conhecimento*')
+
+    select('Linguística, Letras e Artes', from: 'Grande Área de Conhecimento')
+    select('Letras', from: 'Área de Conhecimento*')
+
+    select('Outras', from: 'Grande Área de Conhecimento')
+    select('Biomedicina', from: 'Área de Conhecimento*')
   end
 end
