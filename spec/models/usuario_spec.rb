d6b2d57c4dbd6965edb6d2339bccf586c51b6203
Bernardo B. Marques <bernardo.fire@gmail.com>
2012-02-27 14:29:29 -0300

    removendo campo 'usuario' do teste de model (reparando build) #28

--- spec/models/usuario_spec.rb
+++ spec/models/usuario_spec.rb
@@ -4,11 +4,8 @@ describe Usuario do
   it { should have_valid(:email).when 'bernardo.fire@gmail.com', 'aeiou@abcd.com' }
   it { should_not have_valid(:email).when '', nil }
 
-  it { should have_valid(:usuario).when 'bernardofire', 'C3PO' }
-  it { should_not have_valid(:usuario).when 'han solo', '', nil }
-
-  it { should have_valid(:nome_completo).when 'Luke Skywalker', 'Darth Vader', '', nil }
-  it { should_not have_valid(:nome_completo).when 'r2d2', '123456789' }
+  it { should have_valid(:nome_completo).when 'Luke Skywalker', 'Darth Vader' }
+  it { should_not have_valid(:nome_completo).when 'r2d2', '123456789', nil, '' }
 
   it { should have_valid(:instituicao).when 'ifto', 'IFF' }
   it { should_not have_valid(:instituicao).when '', nil }
