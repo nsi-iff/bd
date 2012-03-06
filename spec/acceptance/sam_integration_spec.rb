21ef5597bccef36c03c5a4b027dd2c392fcee797
Rodrigo Manhães <rmanhaes@gmail.com>
2012-03-05 23:59:09 -0300

    2 espaços bom, tab ruim

--- spec/acceptance/sam_integration_spec.rb
+++ spec/acceptance/sam_integration_spec.rb
@@ -3,18 +3,18 @@
 require 'spec_helper'
 
 feature 'Integração com o SAM' do
-	before(:all) do
-		config = Rails.application.config
-		@sam = NSISam::Client.new "http://#{config.sam_user}:#{config.sam_password}@#{config.sam_host}:#{config.sam_port}"
-	end
+  before(:all) do
+    config = Rails.application.config
+    @sam = NSISam::Client.new "http://#{config.sam_user}:#{config.sam_password}@#{config.sam_host}:#{config.sam_port}"
+  end
 
-	scenario 'padrão', javascript: true  do
-		submeter_conteudo :artigo_de_evento, titulo: 'integracao sam', link: '', arquivo: Rails.root + 'spec/resources/arquivo.nsi'
-		page.should have_content 'com sucesso'
+  scenario 'padrão', javascript: true  do
+    submeter_conteudo :artigo_de_evento, titulo: 'integracao sam', link: '', arquivo: Rails.root + 'spec/resources/arquivo.nsi'
+    page.should have_content 'com sucesso'
 
-		@artigo = ArtigoDeEvento.find_by_titulo('integracao sam')
+    @artigo = ArtigoDeEvento.find_by_titulo('integracao sam')
 
-		response = @sam.get(@artigo.arquivo.key)
-		response.should have_key("data")
-	end
+    response = @sam.get(@artigo.arquivo.key)
+    response.should have_key("data")
+  end
 end
