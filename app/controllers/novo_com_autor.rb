f97b912602468c4c73c3ecf07f72ad34918f932a
Rodrigo Manhães <rmanhaes@gmail.com>
2012-03-14 17:06:33 -0300

    eventos do workflow nos controller isolados no modulo WorkflowActions (#62)

--- app/controllers/novo_com_autor.rb
+++ app/controllers/novo_com_autor.rb
@@ -5,14 +5,5 @@ module NovoComAutor
     attr_name = "@#{model_object_name}"
     instance_variable_set(:"@#{model_object_name}", object)
   end
-
-  private
-
-  def model_class
-    self.class.name.sub('Controller', '').singularize.constantize
-  end
-
-  def model_object_name
-    self.class.name.sub('Controller', '').singularize.underscore
-  end
 end
+
