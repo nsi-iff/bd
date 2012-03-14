4bfd6308b52457c5b719453d5bf40207517681c6
Rodrigo Manhães <rmanhaes@gmail.com>
2012-03-14 17:14:24 -0300

    removida linha inutil

--- app/controllers/novo_com_autor.rb
+++ app/controllers/novo_com_autor.rb
@@ -2,7 +2,6 @@ module NovoComAutor
   def new
     object = model_class.new
     object.autores << Autor.new
-    attr_name = "@#{model_object_name}"
     instance_variable_set(:"@#{model_object_name}", object)
   end
 end
