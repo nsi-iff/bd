47a0321dba3122d572ccf78d9dc6889f23dbb445
Rodrigo Manhães <rmanhaes@gmail.com>
2012-03-16 23:24:11 -0300

    todos os testes, rodados com rack_test ou wekbit, agora compartilham a mesma conexao, voltando a usar transactional fixtures, eliminada a necessidade de rodar o DatabaseCleaner a cada teste (aqui, ganho de 10 segundos)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,8 +10,22 @@ require 'cancan/matchers'
 
 require 'integration/fake_sam' unless ENV["INTEGRACAO"]
 
+# ver http://blog.plataformatec.com.br/2011/12/three-tips-to-improve-the-performance-of-your-test-suite/
 Devise.stretches = 1
 Rails.logger.level = 4
+
+class ActiveRecord::Base
+  mattr_accessor :shared_connection
+  @@shared_connection = nil
+
+  def self.connection
+    @@shared_connection || retrieve_connection
+  end
+end
+
+# Forces all threads to share the same connection. This works on
+# Capybara because it starts the web server in a thread.
+ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -29,20 +43,10 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
 
   config.before :each do
-    if example.metadata[:javascript]
-      Capybara.current_driver = :webkit
-      DatabaseCleaner.strategy = :truncation
-    else
-      DatabaseCleaner.strategy = :transaction
-    end
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
+    Capybara.current_driver = :webkit if example.metadata[:javascript]
   end
 
   # If true, the base class of anonymous controllers will be inferred
