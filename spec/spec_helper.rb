6f18a5aea9a69e0828afe71b0fab79a4203947ba
Rodrigo Manhães <rmanhaes@gmail.com>
2012-03-16 23:02:03 -0300

    pequenas configurações do devise e de logs para diminuir o tempo dos testes (aqui, ganho de 7 segundos)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,9 @@ require 'cancan/matchers'
 
 require 'integration/fake_sam' unless ENV["INTEGRACAO"]
 
+Devise.stretches = 1
+Rails.logger.level = 4
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
