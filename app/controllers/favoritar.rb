37bc02838a922029a00af91bfb60310c61202662
Bernardo B. Marques <bernardo.fire@gmail.com>
2012-03-25 03:33:11 -0300

    não pode favoritar o mesmo conteúdo mais de uma vez (#77)

--- app/controllers/favoritar.rb
+++ app/controllers/favoritar.rb
@@ -1,7 +1,9 @@
 module Favoritar
   def favoritar
     conteudo = Conteudo.find(params["#{model_object_name}_id"])
-    current_usuario.favoritos << conteudo
+    unless current_usuario.favoritos.include? conteudo
+      current_usuario.favoritos << conteudo
+    end
     redirect_to conteudo
   end
 
