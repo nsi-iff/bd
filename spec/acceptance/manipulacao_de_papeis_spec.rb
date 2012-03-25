173d0069542f0dcb52d7a3cf5533805e55d2b055
Bernardo B. Marques <bernardo.fire@gmail.com>
2012-03-25 18:34:25 -0300

    alterando url --> path, no testes de manipulações de papeis

--- spec/acceptance/manipulacao_de_papeis_spec.rb
+++ spec/acceptance/manipulacao_de_papeis_spec.rb
@@ -7,7 +7,7 @@ feature 'mudar papel do usuário' do
     criar_papeis
     autenticar_usuario
 
-    visit '/usuarios'
+    visit usuarios_path
 
     page.should have_content 'Acesso negado'
   end
@@ -16,7 +16,7 @@ feature 'mudar papel do usuário' do
     criar_papeis
     usuario = autenticar_usuario(Papel.admin)
 
-    visit '/usuarios'
+    visit usuarios_path
     check "#{usuario.email}[\"membro\"]"
     check "#{usuario.email}[\"gestor\"]"
     click_button 'Salvar'
@@ -35,7 +35,7 @@ feature 'mudar papel do usuário' do
     Factory.create(:usuario_contribuidor, nome_completo: 'Priscila Manhães', email: 'priscila@manhaes.com')
     Factory.create(:usuario_gestor, nome_completo: 'Larva Fire')
 
-    visit '/usuarios'
+    visit usuarios_path
     fill_in 'Buscar por nome', with: 'Manhães'
     click_button 'Buscar'
 
