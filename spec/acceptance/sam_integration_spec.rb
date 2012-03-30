a9c708eb33ba1211e506b74128849804081ea42a
Douglas Camata <d.camata@gmail.com>
2012-03-30 00:28:09 -0300

    rodando o spec de integracao com SAM somente quando tiver ENV['INTEGRACAO_SAM']

--- spec/acceptance/sam_integration_spec.rb
+++ spec/acceptance/sam_integration_spec.rb
@@ -2,8 +2,9 @@
 
 require 'spec_helper'
 require 'nsisam'
+require 'base64'
 
-feature 'Integração com o SAM' do
+feature 'Integração com o SAM', :if => ENV['INTEGRACAO_SAM'] do
   before(:all) do
     config = Rails.application.config
     @sam = NSISam::Client.new "http://#{config.sam_user}:#{config.sam_password}@#{config.sam_host}:#{config.sam_port}"
@@ -16,6 +17,15 @@ feature 'Integração com o SAM' do
     @artigo = ArtigoDeEvento.find_by_titulo('integracao sam')
 
     response = @sam.get(@artigo.arquivo.key)
+    response.should have(3).keys
+
+    response.should have_key("from_user")
+    response["from_user"].should == Rails.application.config.sam_user
+
+    response.should have_key("date")
+
+    arquivo_64 = Base64.encode64(File.open('spec/resources/arquivo.nsi').read)
     response.should have_key("data")
+    response["data"].should == arquivo_64
   end
 end
