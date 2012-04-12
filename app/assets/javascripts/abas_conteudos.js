4724efb0668a3b2c7c9abeed41821b8ef8644160
Gabriel L. Oliveira <ciberglo@gmail.com>
2012-04-12 15:42:47 -0300

    refatorar abas_conteudos.js para reduzir chamadas repetitivas

--- app/assets/javascripts/abas_conteudos.js
+++ app/assets/javascripts/abas_conteudos.js
@@ -1,15 +1,15 @@
- $(document).ready(function() {
+$(document).ready(function() {
+    // selecionar aba padrao
+    selecionar_aba_de($('.sel_aba[data-padrao]'))
+
     $('.sel_aba').click(function() {
-        show_aba('#' + $(this).data('aba_id'));
-        $(this).addClass('selecionado');
-        $(this).siblings('.sel_aba').removeClass('selecionado');
+        event.preventDefault()
+        selecionar_aba_de($(this))
     });
 
-    var sel_aba_padrao = $('.sel_aba[data-padrao]');
-    show_aba('#' + sel_aba_padrao.data('aba_id'));
-    sel_aba_padrao.addClass('selecionado');
-
-    function show_aba(aba_selector) {
-        $(aba_selector).show().siblings('.aba').hide();
+    function selecionar_aba_de(sel_aba) {
+        sel_aba.addClass('selecionado').siblings('.sel_aba').removeClass('selecionado')
+        var aba = document.getElementById(sel_aba.data('aba_id'))
+        $(aba).show().siblings('.aba').hide()
     };
 });
