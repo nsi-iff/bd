5e09dad6950d4b2e84a3d7dd181565e176fbc903
Gabriel L. Oliveira <ciberglo@gmail.com>
2012-04-13 02:50:18 -0300

    refatorar nome de metodo em javascript para camelCase

--- app/assets/javascripts/abas_conteudos.js
+++ app/assets/javascripts/abas_conteudos.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
     // selecionar aba padrao
-    selecionar_aba_de($('.sel_aba[data-padrao]'))
+    selecionarAbaDe($('.sel_aba[data-padrao]'))
 
     $('.sel_aba').click(function() {
         event.preventDefault()
-        selecionar_aba_de($(this))
+        selecionarAbaDe($(this))
     });
 
-    function selecionar_aba_de(sel_aba) {
+    function selecionarAbaDe(sel_aba) {
         sel_aba.addClass('selecionado').siblings('.sel_aba').removeClass('selecionado')
         var aba_id = sel_aba.children().attr('href')
         $(aba_id).show().siblings('.aba').hide()
