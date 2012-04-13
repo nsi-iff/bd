02e542d459d9fde624a3a022d221fa78733c0e95
Gabriel L. Oliveira <ciberglo@gmail.com>
2012-04-13 00:21:14 -0300

    refatorar para reduzir código e adaptá-lo ao novo comportamento, onde o ano (radio button) já está selecionado por padrão

--- app/assets/javascripts/estatistica.js
+++ app/assets/javascripts/estatistica.js
@@ -1,16 +1,11 @@
-//TODO: colocar ano selecionado por padrao (na view ruby) (2012-04-12, 17:39, ciberglo)`
-//TODO: refatorar (2012-04-12, 17:39, ciberglo)`
-//TODO: remover funções do contexto global (2012-04-12, 23:37, ciberglo)`
-function ano()
-{
-    if ($('#select_mes')[0].disabled == false)
-        $('#select_mes')[0].disabled = true;
-    $('#select_ano')[0].disabled = false;
-}
+//TODO: colocar esse script para so carregar na pagina de estatisticas.html.erb e pensar em um nome melhor para "radio_[ano..]" (2012-04-13, 00:10, ciberglo)`
 
-function mes()
-{
-    if ($('#select_ano')[0].disabled == true)
-        $('#select_ano')[0].disabled = false;
-    $('#select_mes')[0].disabled = false;
-}
+$(document).ready(function(){
+    $('#radio_ano').click(function(){
+        $('#select_mes').attr('disabled', true)
+    })
+
+    $('#radio_mes').click(function(){
+        $('#select_mes').removeAttr('disabled')
+    })
+});
