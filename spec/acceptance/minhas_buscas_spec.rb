12150d1bba6a8db1b05528c49f7c27460dd0a588
Pedro Henrique Linhares <pedrolmota@gmail.com>
2012-04-16 16:02:48 -0300

    tentativa consertar erro ao enviar email mala direta. #36

--- spec/acceptance/minhas_buscas_spec.rb
+++ spec/acceptance/minhas_buscas_spec.rb
@@ -88,21 +88,18 @@ feature 'Buscas' do
      ActionMailer::Base.deliveries.should be_empty
 
      #mudar hora para 2 horas da manha do dia seguinte
-     amanha = Time.now + 1.day
-     amanha_as_2_horas = amanha.strftime('%Y-%m-%d') + ' 2:00 am'
+     amanha_as_2_horas = Date.tomorrow.strftime('%Y-%m-%d') + ' 2:00 am'
+
      Delorean.time_travel_to amanha_as_2_horas
      sleep(5)
 
-     # TODO: resolver isto!
-     #Comentado porque esta quebrando o BUILD porem esta passando na minha maquina
-     #verificar se emails foram disparados
-     # ActionMailer::Base.deliveries.should_not be_empty
-     # ActionMailer::Base.deliveries.size.should == 1
+     ActionMailer::Base.deliveries.should_not be_empty
+     ActionMailer::Base.deliveries.size.should == 1
 
-     # email = ActionMailer::Base.deliveries.last
+     email = ActionMailer::Base.deliveries.last
 
-     # email.to.should == [usuario.email]
-     # email.subject.should == 'Biblioteca Digital: Novos documentos de seu interesse'
+     email.to.should == [usuario.email]
+     email.subject.should == 'Biblioteca Digital: Novos documentos de seu interesse'
 
      Delorean.back_to_the_present
   end
