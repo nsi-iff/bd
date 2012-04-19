f4aee6cbaec5e606d63c802e184f9d945ca5f80c
Pedro Henrique Linhares <pedrolmota@gmail.com>
2012-04-19 17:56:14 -0300

    corrigida sintaxe errada das tarefas cron para serem executadas todos os dias.

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,6 +1,6 @@
 scheduler = Rufus::Scheduler.start_new
 
-scheduler.cron '0 58 23 * * 1-5' do
+scheduler.cron '0 58 23 * * 1-7' do
   acesso = Acesso.new
   if Rails.env == 'test'
     acesso.log_file = "#{Rails.root}/spec/resources/access.log"
@@ -8,6 +8,6 @@ scheduler.cron '0 58 23 * * 1-5' do
   acesso.save
 end
 
-scheduler.cron '0 00 2 * * 1-5' do
+scheduler.cron '0 00 2 * * 1-7' do
   Busca.enviar_email_mala_direta
 end
