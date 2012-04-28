ddde0f88e0618a493f6ba7c0ebb20c17f940e7c5
Bernardo B. Marques <bernardo.fire@gmail.com>
2012-04-28 03:10:57 -0300

    teste de controle de acesso cobrindo aprensentação de conteúdo

--- spec/acceptance/controle_de_acesso_spec.rb
+++ spec/acceptance/controle_de_acesso_spec.rb
@@ -3,6 +3,33 @@
 require 'spec_helper'
 
 feature 'controle de acesso' do
+
+  context 'ver conteúdo' do
+    scenario 'qualquer usuário inclusive convidados podem ver conteúdo publicado' do
+      criar_papeis
+      livro = Factory.create(:livro)
+      livro.submeter!
+      livro.aprovar!
+
+      visit conteudo_path(livro)
+
+      page.should have_content 'Metadados'
+      page.should_not have_content 'Acesso negado'
+    end
+
+    scenario 'apenas dono do conteúdo pode vê-lo em estado editavel' do
+      usuario1 = autenticar_usuario(Papel.contribuidor)
+      livro = Factory.create(:livro, contribuidor: usuario1)
+
+      visit conteudo_path(livro)
+      page.should have_content 'Metadados'
+
+      autenticar_usuario
+      visit conteudo_path(livro)
+      page.should_not have_content 'Metadados'
+      page.should have_content 'Acesso negado'
+    end
+  end
   context 'adicionar conteudos' do
     scenario 'pode ser acessado por contribuidores de conteúdo' do
       criar_papeis
