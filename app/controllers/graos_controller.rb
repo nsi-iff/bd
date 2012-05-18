f557695d947c8be88b59b3ebb2acca971a9f4912
Rômulo Machado <romulo.machado5@gmail.com>
2012-05-18 16:54:37 -0300

    adicionado método baixar_conteudo ao controller #50

--- app/controllers/graos_controller.rb
+++ app/controllers/graos_controller.rb
@@ -1,3 +1,5 @@
+require './lib/zip_entry.rb'
+
 class GraosController < ApplicationController
   before_filter :carregar_grao, :if => :current_usuario
 
@@ -11,6 +13,30 @@ class GraosController < ApplicationController
     @cesta.delete(params[:id])
     current_usuario.cesta.delete(@grao) if current_usuario.present?
     respond_to &:js
+  end
+
+  def baixar_conteudo
+    unless current_usuario.cesta.blank?
+      @sam = ServiceRegistry.sam
+      t = Tempfile.new("cesta_temporaria")
+      Zip::ZipOutputStream.open(t.path) do |z|
+        current_usuario.cesta.all.map(&:key).each_with_index do |key, index|
+          objeto_grao = Grao.where(:key => key).first
+          tipo_grao = objeto_grao.tipo
+          conteudo_id = objeto_grao.conteudo_id
+          title = 'grao_' + Conteudo.where(:id => conteudo_id).first.titulo +  '_' + index.to_s
+          if tipo_grao == 'files' then title += '.odt' else title += '.jpg' end
+          grao = @sam.get(key)
+          grao = Base64.decode64(grao['data'])
+          z.put_next_entry(title)
+          z.print grao
+        end
+      end
+    end
+    send_file t.path, :type => 'application/zip',
+                      :disposition => 'attachment',
+                      :filename => 'cesta-' + Time.now.to_s + '.zip'
+    t.close
   end
 
   def cesta
