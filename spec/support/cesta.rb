05050c4b4de99c54556b6857be8b08fa05740cf9
Rodrigo Manhães <rmanhaes@gmail.com>
2012-05-18 20:23:02 -0300

    diminuido tempo de sleep da cesta de graos

--- spec/support/cesta.rb
+++ spec/support/cesta.rb
@@ -9,7 +9,7 @@ def criar_cesta(usuario, conteudo, *grain_files)
   sam = ServiceRegistry.sam
   grain_files.each do |file|
     result = sam.store(Base64.encode64(File.read(file)))
-    sleep(3)
+    sleep(1)
     tipo_grao = file.downcase.end_with?('odt') ? :grao_arquivo : :grao_imagem
     grao = FactoryGirl.create(tipo_grao, key: result['key'], conteudo: conteudo)
     usuario.cesta << grao
