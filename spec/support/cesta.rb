b1ff21226020e470227e44424d051090b9722d15
Rômulo Machado <romulo.machado5@gmail.com>
2012-05-18 16:56:36 -0300

    adicionados testes para download de conteudo da cesta. closes #50

--- spec/support/cesta.rb
+++ spec/support/cesta.rb
@@ -1,14 +1,15 @@
+# encoding: utf-8
 require 'base64'
 
 def item_da_cesta(n)
   "#cesta #items div:nth-child(%s)" % n
 end
 
-def criar_cesta(usuario, *grain_files)
-  conteudo = FactoryGirl.create(:relatorio)
+def criar_cesta(usuario, conteudo, *grain_files)
   sam = ServiceRegistry.sam
   grain_files.each do |file|
-    result = sam.store(doc: Base64.encode64(File.read(file)))
+    result = sam.store(Base64.encode64(File.read(file)))
+    sleep(3)
     tipo_grao = file.downcase.end_with?('odt') ? :grao_arquivo : :grao_imagem
     grao = FactoryGirl.create(tipo_grao, key: result['key'], conteudo: conteudo)
     usuario.cesta << grao
