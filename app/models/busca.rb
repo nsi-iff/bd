296fdf6981305578657ad115901d57cc62345c55
Natanael Araújo Silva <internael@gmail.com>
2012-05-22 19:09:39 -0300

    refactoring em Busca

--- app/models/busca.rb
+++ app/models/busca.rb
@@ -1,33 +1,26 @@
 class Busca < ActiveRecord::Base
   belongs_to :usuario
-
   validates :titulo, presence: true
 
   def self.enviar_email_mala_direta
     ontem = Date.yesterday.strftime("%d/%m/%y")
-    conteudos_por_usuarios = {}
+    conteudos_por_usuarios = Hash.new { |hash, email| hash[email] = [] }
 
     self.where(mala_direta: true).each do |busca|
-      conteudos = []
-      s = Tire.search 'conteudos' do
-        query { string busca.busca }
-        filter :term, :data_publicado => ontem
-      end
-      s.results.each {|conteudo| conteudos << conteudo }
-
-      if conteudos.present?
-        if conteudos_por_usuarios.has_key?(busca.usuario.email)
-          conteudos.each do |conteudo|
-            conteudos_por_usuarios[busca.usuario.email] << conteudo
-          end
-        else
-          conteudos_por_usuarios[busca.usuario.email] = conteudos
-        end
-      end
+      conteudos = busca.resultados(:data_publicado => ontem)
+      conteudos_por_usuarios[busca.usuario.email].concat conteudos if conteudos.present?
     end
 
     conteudos_por_usuarios.each_pair do |usuario_email, conteudos|
       Mailer.notificar_usuario_sobre_conteudos(usuario_email, conteudos).deliver
     end
+  end
+
+  def resultados(filtros = nil)
+    busca = self.busca
+    Tire.search('conteudos') {
+      query { string busca }
+      filter(:term, filtros) if filtros
+    }.results.to_a
   end
 end
