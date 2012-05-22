85ec701e6ef48b1c987e3fd796b143648ddad74e
Rodrigo Manhães <rmanhaes@gmail.com>
2012-05-22 20:42:23 -0300

    foo

--- app/models/livro.rb
+++ app/models/livro.rb
@@ -2,4 +2,8 @@ class Livro < Conteudo
   index_name 'conteudos'
   validates :numero_paginas, :numero_edicao,
     numericality: { greater_than: 0, allow_blank: true }
+
+  def self.nome_humanizado
+    'Livro'
+  end
 end
