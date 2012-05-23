a2e7db8c3020afa42273b910553437121133d62b
Douglas Camata <d.camata@gmail.com>
2012-05-22 22:25:16 -0300

    registering the cloudooo service too

--- config/initializers/service_registry.rb
+++ config/initializers/service_registry.rb
@@ -6,12 +6,24 @@ class ServiceRegistry
   def self.sam
     @sam ||= NSISam::Client.new(Rails.application.config.sam_configuration)
   end
+
+  def self.cloudooo=(cloudooo)
+    @cloudooo = cloudooo
+  end
+
+  def self.cloudooo
+    @cloudooo ||= NSICloudooo::Client.new(Rails.application.config.cloudooo_configuration)
+  end
 end
 
 module ActiveRecord
   class Base
     def sam
       ServiceRegistry.sam
+    end
+
+    def cloudooo
+      ServiceRegistry.cloudooo
     end
   end
 end
