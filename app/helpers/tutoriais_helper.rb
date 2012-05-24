8b44fc2f77b29651a31d032e5094c315040bd8b0
Pedro Henrique Linhares <pedrolmota@gmail.com>
2012-05-24 15:56:28 -0300

    adicionado helper para url de imagem de dos tutoriais #101

--- app/helpers/tutoriais_helper.rb
+++ app/helpers/tutoriais_helper.rb
@@ -2,4 +2,12 @@ module TutoriaisHelper
   def url_para(item)
     "#{url_for}/#{item}"
   end
+
+  def url_imagem(imagem)
+    "/imagens/#{imagem}"
+  end
+
+  def url_tutorial(tutorial)
+    "#{tutoriais_path}/#{tutorial}"
+  end
 end
