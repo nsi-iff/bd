07d3192571b3f02f2fa96e7604594e184f0964d7
Natanael Araújo Silva <internael@gmail.com>
2012-05-28 14:48:02 -0300

    aumentando tempo de espera de indexação

--- spec/acceptance/minhas_buscas_spec.rb
+++ spec/acceptance/minhas_buscas_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 feature 'Buscas' do
   before(:each) do
-    Tire.criar_indices and sleep(1) if ENV['INTEGRACAO_TIRE']
+    Tire.criar_indices and sleep(3) if ENV['INTEGRACAO_TIRE']
   end
 
   before(:each) do
