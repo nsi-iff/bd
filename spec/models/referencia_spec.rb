3dd14e0132558bf69a9573228e6c08dc947fc966
Pedro Henrique Linhares <pedrolmota@gmail.com>
2012-06-06 08:58:51 -0300

    adicionadas validacoes para referencia e atribuidos os valores de abnt e tipo_do_grao quando necessarias #85

--- spec/models/referencia_spec.rb
+++ spec/models/referencia_spec.rb
@@ -4,4 +4,25 @@ describe Referencia do
   it { should_not have_valid(:referenciavel).when nil }
   it { should_not have_valid(:usuario).when nil }
   it { should_not have_valid(:abnt).when nil }
+
+  it 'deve salvar refencia abnt do referenciavel quando criado' do
+    conteudo = create(:artigo_de_periodico)
+    grao = create(:grao, conteudo: conteudo)
+    referencia = create(:referencia, referenciavel: grao)
+    referencia.should be_valid
+    referencia.abnt.should == grao.referencia_abnt
+  end
+
+  it 'nao deve exigir referenciavel se referencia ja existir' do
+    referencia = create(:referencia)
+    referencia.referenciavel = nil
+    referencia.should be_valid
+  end
+
+  it 'deve salvar o tipo do grao se referenciavel for Grao' do
+    conteudo = create(:artigo_de_periodico)
+    grao = create(:grao, conteudo: conteudo)
+    referencia = create(:referencia, referenciavel: grao)
+    referencia.tipo_do_grao.should == grao.tipo
+  end
 end
