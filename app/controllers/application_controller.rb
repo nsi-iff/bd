a7bfef6264408b960c530e3128851488111f2064
Rodrigo Manhães <rmanhaes@gmail.com>
2012-06-12 08:59:13 -0300

    definindo dinamicamente o host para o mailer (#118)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :carregar_cesta
+  before_filter :definir_host_do_mailer
 
   private
+
+  def definir_host_do_mailer
+    ActionMailer::Base.default_url_options[:host] = request.host_with_port
+  end
 
   def carregar_cesta
     @cesta = current_usuario.present? ?
