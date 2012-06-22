71d32e9249f766d7c70f3615b2c057c4783f9f94
joaofeliperm <jfelipe.roque@gmail.com>
2012-06-22 10:41:15 -0300

    referenciados grãos de arquivo aberto em editor wysiwyg closes #75

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -6,10 +6,23 @@ class EditorController < ApplicationController
   end
 
   def download
+    graos = params['graos'].keys[0].to_sym
     documento = File.new("#{Rails.root}/tmp/documento.html", 'w')
     documento.write(params[:documento])
-    documento.close
+    if graos == :true
+      documento.write("<b>")
+      documento.write("REFERÊNCIAS".to_xs)
+      documento.write("</b>")
+      2.times{ documento.write("<br>") }
+      current_usuario.cesta.each do |grao|
+        conteudo_do_grao = Conteudo.find(grao.conteudo_id)
+        documento.write(conteudo_do_grao.referencia_abnt)
+        documento.write("<br>")
+      end
+    end
+    documento.close()
     send_file("#{Rails.root}/tmp/documento.html", :filename => 'documento.html',
     :type => 'text/html', :disposition => 'attachment')
   end
 end
+
