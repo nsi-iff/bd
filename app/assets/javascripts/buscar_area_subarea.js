76f0450ba1caf2658d223e040827ad6064ef7c99
Bernardo B. Marques <bernardo.fire@gmail.com>
2012-06-27 16:45:40 -0300

    layout para busca de subarea (closes #157)

--- app/assets/javascripts/buscar_area_subarea.js
+++ app/assets/javascripts/buscar_area_subarea.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('ul.areas span').live('click', function() {
+        $('.portlet').css('width', '185px');
         var id = $(this).attr('id')
         $('.' + id + '_sub_area').toggle('slow');
     });
