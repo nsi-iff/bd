05eabf16c79aaaa026a8650b695d2c1132beef9c
Rômulo Machado <romulo.machado5@gmail.com>
2012-06-29 16:59:13 -0300

    Criar critérios de validação para campos de metadados de conteúdos #148

--- app/models/artigo_de_periodico.rb
+++ app/models/artigo_de_periodico.rb
@@ -4,12 +4,24 @@ class ArtigoDePeriodico < Conteudo
   index_name 'conteudos'
   validates :volume_publicacao, numericality: { greater_than: 0, allow_blank: true}
   flexible_date :data_publicacao, suffix: 'br'
-  
+
   attr_accessible :nome_periodico, :editora, :fasciculo, :volume_publicacao, 
                   :data_publicacao_br, :local_publicacao, :pagina_inicial, 
                   :pagina_final
+  validate :verificar_paginas
 
   def self.nome_humanizado
     "Artigo de Periódico"
+  end
+
+
+  private
+
+  def verificar_paginas
+    unless pagina_inicial.blank?
+      if pagina_final < pagina_inicial
+        errors.add(:pagina_final, "Página final deve ser maior que página inicial")
+      end
+    end
   end
 end
