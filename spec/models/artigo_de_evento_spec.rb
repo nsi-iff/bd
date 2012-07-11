a500c4986d2c2383fb4455080d77ef68d9f85366
Bernardo B. Marques <bernardo.fire@gmail.com>
2012-07-11 16:45:06 -0300

    adicionando teste para pagina inicial e final de artigo de evento (#148)

--- spec/models/artigo_de_evento_spec.rb
+++ spec/models/artigo_de_evento_spec.rb
@@ -3,4 +3,12 @@ require 'spec_helper'
 describe ArtigoDeEvento do
   it { should have_valid(:ano_publicacao).when 1990, 2000 }
   it { should_not have_valid(:ano_publicacao).when 1989, 1900 }
+
+  it "pagina final tem que ser maior que pagina final" do
+    artigo_1 = ArtigoDeEvento.new pagina_inicial: 10
+    artigo_1.should have_valid(:pagina_final).when 20
+
+    artigo_2 = ArtigoDeEvento.new pagina_inicial: 30
+    artigo_2.should_not have_valid(:pagina_final).when 20
+  end
 end
