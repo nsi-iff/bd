be73f068fae4360eb065be95169825096d6c51ce
Natanael Araújo Silva <internael@gmail.com>
2012-07-23 14:07:34 -0300

    Carrega o mock do elasticsearch antes dos models

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,8 +26,18 @@ Spork.prefork do
   # I.e., prevent Rails::Application to loads files in the application autoload_paths
   # such as app/models, app/controllers, etc.
   Spork.trap_method(Rails::Application, :eager_load!)
+
+  require "tire"
+  require "tire-mock_client"
+  Tire.configure do
+    unless ENV['INTEGRACAO_TIRE']
+      client Tire::Http::Client::MockClient
+    end
+  end
+
   # Depois dessa linha já é tarde demais. Now, load the rails stack
   require File.expand_path("../../config/environment", __FILE__)
+
   # Load all railties files, i.e., eager load all the engines
   Rails.application.railties.all { |r| r.eager_load! }
 
@@ -99,12 +109,6 @@ Spork.prefork do
     # automatically. This will be the default behavior in future versions of
     # rspec-rails.
     config.infer_base_class_for_anonymous_controllers = false
-  end
-
-  Tire.configure do
-    unless ENV['INTEGRACAO_TIRE']
-      client Tire::Http::Client::MockClient
-    end
   end
 end
 
