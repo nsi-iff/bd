2359c788a6c447878dbdfe9a47bad0e40e9a8238
Natanael Araújo Silva <internael@gmail.com>
2012-07-23 23:40:08 -0300

    cesta_de_graos_spec rodando com integração

--- spec/acceptance/cesta_de_graos_spec.rb
+++ spec/acceptance/cesta_de_graos_spec.rb
@@ -3,26 +3,29 @@
 require 'spec_helper'
 
 feature 'cesta de grãos' do
-  before :all do
-    require File.join(Rails.root, *%w(db criar_indices)) if ENV['INTEGRACAO_TIRE']
 
-    # monkeypatch temporario para passar no teste sem o Elastic Search
-    # TODO: remover depois que funcionarem buscas básicas com o ES fake
-    class << Conteudo
-      alias old_search search
-      def search(param)
-        where('upper(titulo) like ?', "%#{param.to_s.upcase}%")
+  unless ENV['INTEGRACAO_TIRE']
+    before :all do
+      # monkeypatch temporario para passar no teste sem o Elastic Search
+      # TODO: remover depois que funcionarem buscas básicas com o ES fake
+
+      class << Conteudo
+        alias old_search search
+        def search(param)
+          where('upper(titulo) like ?', "%#{param.to_s.upcase}%")
+        end
+      end
+    end
+
+    after :all do
+      class << Conteudo
+        alias search old_search
       end
     end
   end
 
-  after :all do
-    class << Conteudo
-      alias search old_search
-    end
-  end
-
   before(:each) do
+    Tire.criar_indices if ENV['INTEGRACAO_TIRE']
     @livro = create(:livro, titulo: 'Quantum Mechanics for Dummies')
     @grao1 = create(:grao_imagem, key: '12345', conteudo: @livro)
     @grao2 = create(:grao_arquivo, key: '67890', conteudo: @livro)
