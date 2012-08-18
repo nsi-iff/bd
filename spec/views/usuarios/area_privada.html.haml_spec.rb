30a95641c1fbdfb393b77b43414cf5e3cfc1db3e
Rodrigo Manhães <rmanhaes@gmail.com>
2012-08-17 23:56:23 -0300

    testes de aceitacao de links na area privada movidos para testes unitarios de acesso em spec/models/usuario e de links na view em spec/views/usuarios/area_privada (#237)

--- spec/views/usuarios/area_privada.html.haml_spec.rb
+++ spec/views/usuarios/area_privada.html.haml_spec.rb
@@ -5,31 +5,48 @@ require 'spec_helper'
 describe "usuarios/area_privada.html.haml" do
   before :each do
     Papel.criar_todos
-    @usuario = create(:usuario_gestor)
-    sign_in :usuario, @usuario
   end
 
-  context 'contem link para' do
-    before(:each) { render }
+  %w(contribuidor admin instituicao_admin gestor).each do |papel|
+    context "links para usuario #{papel}" do
+      before(:each) do
+        @usuario = create(:"usuario_#{papel}")
+        sign_in :usuario, @usuario
+      end
 
-    it 'escrivaninha' do
-      rendered.should have_link 'Escrivaninha',
-        href: escrivaninha_usuario_path(@usuario)
-    end
+      it 'escrivaninha' do
+        render
+        rendered.should have_link 'Escrivaninha',
+          href: escrivaninha_usuario_path(@usuario)
+      end
 
-    it 'estante' do
-      rendered.should have_link 'Estante',
-        href: estante_usuario_path(@usuario)
-    end
+      it 'estante' do
+        render
+        rendered.should have_link 'Estante',
+          href: estante_usuario_path(@usuario)
+      end
 
-    it 'minhas buscas' do
-      rendered.should have_link 'Minhas Buscas',
-        href: minhas_buscas_usuario_path(@usuario)
-    end
+      it 'minhas buscas' do
+        render
+        rendered.should have_link 'Minhas Buscas',
+          href: minhas_buscas_usuario_path(@usuario)
+      end
 
-    it 'lista de revisão' do
-      rendered.should have_link 'Lista de Revisão',
-        href: lista_de_revisao_usuario_path(@usuario)
+      let(:lista_de_revisao) { 'Lista de Revisão' }
+
+      if papel == 'gestor'
+        it 'lista de revisão' do
+          render
+          rendered.should have_link lista_de_revisao,
+            href: lista_de_revisao_usuario_path(@usuario)
+        end
+      else
+        it 'não possui lista de revisão' do
+          sign_in :usuario, create(:usuario_contribuidor)
+          render
+          rendered.should_not have_link lista_de_revisao
+        end
+      end
     end
   end
 end
