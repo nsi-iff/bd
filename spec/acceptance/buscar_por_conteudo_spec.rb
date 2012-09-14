23a7c2ac02b3793b6ec27018088d334acde738f2
Felipe Norato Lacerda <fnoratol@gmail.com>
2012-09-14 16:02:21 -0300

    Correção do teste de Busca por Conteudo #265

---
+++ spec/acceptance/buscar_por_conteudo_spec.rb
@@ -0,0 +1,112 @@
+# encoding: UTF-8
+
+require "spec_helper"
+
+feature 'buscar todos os tipos de conteúdo' do
+
+  before :each do
+    Tire.criar_indices
+    iff = Instituicao.create(nome: 'IFF')
+    campos_centro = Campus.create nome: 'Campos Centro', instituicao: iff
+    cabo_frio = Campus.create nome: 'Cabo Frio', instituicao: iff
+    autor_1       = Autor.create nome: 'Yukihiro Matsumoto',
+                                 lattes: "http://lattes.cnpq.br/1234567890"
+    autor_2       = Autor.create nome: "Why, the Lucky Stiff",
+                                 lattes: "lattes.cnpq.br/6666666666"
+    grande_area_1 = Area.create nome: "Ciências Exatas e da Terra"
+    grande_area_2 = Area.create nome: "Engenharia"
+    sub_area_1    = grande_area_1.sub_areas.create nome: "Ciência da Computação"
+    sub_area_2    = grande_area_2.sub_areas.create nome: "Engenharia Nuclear"
+    @artigo_de_evento = ArtigoDeEvento.create titulo: "Artigo de Evento",
+                                              link: "http://www.rubyconf.org/articles/1",
+                                              sub_area: sub_area_2,
+                                              campus: campos_centro,
+                                              autores: [autor_1]
+    @artigo_de_periodico = ArtigoDePeriodico.create titulo: "Artigo de Periódico",
+                                                    link: "http://nsi.iff.edu.br",
+                                                    sub_area: sub_area_1,
+                                                    campus: campos_centro,
+                                                    autores: [autor_1, autor_2],
+                                                    volume_publicacao: 10
+    @livro = Livro.create titulo: "Livro",
+                          link: "",
+                          arquivo: Arquivo.new(
+                            uploaded_file: ActionDispatch::Http::UploadedFile.new({
+                              filename: 'arquivo.rtf',
+                              type: 'text/rtf',
+                              tempfile: File.new(Rails.root + 'spec/resources/arquivo.rtf')
+                            })),
+                          sub_area: sub_area_1,
+                          campus: campos_centro,
+                          autores: [autor_1, autor_2]
+    @objeto_de_aprendizagem = ObjetoDeAprendizagem.create titulo: "Objeto de Aprendizagem",
+                                                          link: "http://www.rubyconf.org/articles/1",
+                                                          sub_area: sub_area_2,
+                                                          campus: campos_centro,
+                                                          autores: [autor_1]
+    @periodico_tecnico_cientifico = PeriodicoTecnicoCientifico.create titulo: "Periodico Tecnico Cientifico",
+                                                                       link: "http://www.rubyconf.org/articles/1",
+                                                                       sub_area: sub_area_2,
+                                                                       campus: cabo_frio,
+                                                                       autores: [autor_1]
+    @relatorio = Relatorio.create titulo: "Relatório",
+                                   link: "http://www.rubyconf.org/articles/1",
+                                   sub_area: sub_area_1,
+                                   campus: campos_centro,
+                                   autores: [autor_2]
+    @trabalho_de_obtencao_de_grau = TrabalhoDeObtencaoDeGrau.create titulo: "Trabalho de Obtencao de Grau",
+                                                                     link: "http://www.rubyconf.org/articles/1",
+                                                                     sub_area: sub_area_2,
+                                                                     campus: campos_centro,
+                                                                     autores: [autor_1]
+
+    aprovar(@artigo_de_evento, @artigo_de_periodico,
+            @livro, @objeto_de_aprendizagem,
+            @periodico_tecnico_cientifico,
+            @relatorio, @trabalho_de_obtencao_de_grau)
+    Conteudo.tire.index.refresh
+    Arquivo.tire.index.refresh
+  end
+
+  scenario 'por título' do
+    testar_busca "Artigo", @artigo_de_evento, @artigo_de_periodico
+  end
+
+  scenario 'por link' do
+    testar_busca "nsi.iff.edu.br", @artigo_de_periodico
+  end
+
+  scenario 'por área' do
+    testar_busca "Ciências Exatas e da Terra", @livro, @artigo_de_periodico, @relatorio
+  end
+
+  scenario 'por sub area' do
+    testar_busca "Ciencia da Computação", @livro, @artigo_de_periodico, @relatorio
+  end
+
+  scenario 'por campus' do
+    testar_busca "Cabo Frio", @periodico_tecnico_cientifico
+  end
+
+  scenario 'pesquisar por nome de autor' do
+    testar_busca "Why, the Lucky Stiff", @livro, @artigo_de_periodico, @relatorio
+  end
+
+  scenario 'pesquisar por Lattes de autor' do
+    testar_busca "lattes.cnpq.br/6666666666", @livro, @artigo_de_periodico, @relatorio
+  end
+
+  scenario 'pesquisar por arquivo' do
+    testar_busca "Livro", @livro
+  end
+
+  def testar_busca(texto, *resultados)
+    visit root_path
+    fill_in "text_busca_inicio", with: texto
+    click_button "Buscar"
+    resultados.each do |resultado|
+      page.should have_link resultado.titulo,
+                            href: conteudo_path(resultado)
+    end
+  end
+end
