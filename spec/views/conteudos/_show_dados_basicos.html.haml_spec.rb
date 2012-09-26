1fdd5d522fcd63ee269066992a5a6050079811c6
Douglas Camata <d.camata@gmail.com>
2012-09-26 19:19:09 -0300

    spec da view de dados básicos de conteudo atualizada para download direto do sam

--- spec/views/conteudos/_show_dados_basicos.html.haml_spec.rb
+++ spec/views/conteudos/_show_dados_basicos.html.haml_spec.rb
@@ -7,24 +7,28 @@ describe "conteudos/_show_dados_basicos.html.haml" do
     Papel.criar_todos
   end
 
-  describe 'botão de download' do
+  describe 'link de download' do
     before(:each) do
       view.stub(:can?).and_return(true)
       @conteudo = create(:relatorio_publicado)
       view.stub(:conteudo).and_return(@conteudo)
       sign_in :usuario, create(:usuario_gestor)
     end
-    
+
     it 'aparece para documentos disponiveis para download' do
+      @conteudo.stub(:link_download).and_return('foo')
       @conteudo.stub(:disponivel_para_download?).and_return(true)
+      # @conteudo.stub_chain(:arquivo, :key).and_return('foo')
+      # @conteudo.stub_chain(:arquivo, :thumbnail).and_return('bar')
+      # @conteudo.stub_chain(:arquivo, :nome).and_return('arquivo.rtf')
       render
-      rendered.should have_button 'Download'
+      rendered.should have_link 'Download'
     end
-    
+
     it 'nao aparece para documentos nao disponiveis para download' do
       @conteudo.stub(:disponivel_para_download?).and_return(false)
       render
-      rendered.should_not have_button 'Download'
+      rendered.should_not have_link 'Download'
     end
   end
 end
