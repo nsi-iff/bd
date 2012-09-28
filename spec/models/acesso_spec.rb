28d68fada1d5078c9080494fab0ce6b2110388c8
Natanael Araújo Silva <internael@gmail.com>
2012-09-27 23:09:17 -0300

    aumenta o tempo de espera em teste problemático de mala-direta

--- spec/models/acesso_spec.rb
+++ spec/models/acesso_spec.rb
@@ -25,7 +25,7 @@ describe Acesso do
   it 'faz a contagem de acesso diariamente as 23:58' do
     Acesso.count.should == 0
     Delorean.time_travel_to Date.today.strftime('%Y-%m-22') + ' 11:58 pm'
-    sleep(0.5)
+    sleep(1)
     Acesso.total_de_acessos.should == 3
     Delorean.back_to_the_present
   end
