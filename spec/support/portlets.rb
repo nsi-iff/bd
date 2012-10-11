cc5f5305c3ac0e0f3b3b4181bd282b8af2ffc056
Rodrigo Manhães <rmanhaes@gmail.com>
2012-10-11 15:20:08 -0300

    teste de limitacao da quantidade de itens na lista de revisao movido para um shared_examples_for (#271)

--- spec/support/portlets.rb
+++ spec/support/portlets.rb
@@ -7,3 +7,17 @@ end
 def estante_vazia
   'Não há nenhum conteúdo pendente.'
 end
+
+shared_examples_for 'a portlet' do
+  it 'mostra apenas um número fixo de itens' do
+    Rails.application.config.limite_de_itens_nos_portlets = 4
+    conteudos_para_a_lista = (1..8).map do |n| 
+      FactoryGirl.create(:conteudo, id: n, titulo: "Conteudo #{n}", created_at: Time.now)
+    end
+    view.stub(:current_usuario).and_return(stub_usuario[conteudos_para_a_lista])
+    view.stub(:can?).and_return(true)
+    render
+    (1..4).each {|n| rendered.should have_content "Conteudo #{n}" }
+    (5..6).each {|n| rendered.should_not have_content "Conteudo #{n}" }
+  end
+end
