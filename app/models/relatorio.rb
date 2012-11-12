80bcb9a1e7fb75e55235c9651b56f3ca26b5b986
Bernardo B. Marques <bernardo.fire@gmail.com>
2012-11-12 16:39:38 -0200

    adicionando validação para campos numéricos em relatório (#321)

--- app/models/relatorio.rb
+++ app/models/relatorio.rb
@@ -4,8 +4,8 @@ class Relatorio < Conteudo
   index_name 'conteudos'
   validates :numero_paginas, numericality: { greater_than: 0, allow_blank: true }
   validates :ano_publicacao,
-    numericality: { less_than_or_equal_to: Time.now.year, allow_blank: true }
-    
+    numericality: { greater_than: 0, less_than_or_equal_to: Time.now.year, allow_blank: true }
+
   attr_accessible :local_publicacao, :ano_publicacao, :numero_paginas
 
   def self.nome_humanizado
