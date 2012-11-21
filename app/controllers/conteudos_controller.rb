675d194a8b4fd5d06dce04eb4c97d5174b1c767b
João Felipe <jfelipe.roque@gmail.com>
2012-11-21 17:12:07 -0200

    alterado nome do arquivo ao ser baixado

--- app/controllers/conteudos_controller.rb
+++ app/controllers/conteudos_controller.rb
@@ -167,7 +167,7 @@ class ConteudosController < ApplicationController
     sam = ServiceRegistry.sam
     string64 = sam.get(key)
     arquivo = Base64.decode64(string64.data['file'])
-    file_name  = "#{Rails.root}/tmp/#{@conteudo.titulo}"
+    file_name  = "#{Rails.root}/tmp/#{@conteudo.arquivo.nome}"
     file = File.new(file_name, "w")
     file.write(arquivo.force_encoding('UTF-8'))
     send_file file_name
