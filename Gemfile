10bd30f0698f701466ac4456f50672313fb598b6
Bernardo B. Marques <bernardo.fire@gmail.com>
2012-11-30 17:20:18 -0200

    Enviar e-mail para a administração do site na ocorrência de erros (closes #281)

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.8'
 
+gem 'exception_notification'
 gem 'devise', '~>2.1.0'
 gem 'cancan', '~>1.6.7'
 gem 'pg', '~> 0.14.0'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,39 @@
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :carregar_cesta
   before_filter :definir_host_do_mailer
 
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from Exception, :with => :render_error
+  end
+
+  def render_error(exception)
+    if exceptions_404.include? exception.class
+      log_exception(exception)
+      render :template => "/errors/404", layout: 'application'
+    else
+      log_exception(exception)
+      ExceptionNotifier::Notifier.exception_notification(request.env, exception)
+      render :template => "/errors/500", layout: 'application'
+    end
+  end
+
+  def routing_error
+    raise ActionController::RoutingError.new(params[:path])
+  end
+
   private
+
+  def exceptions_404
+    [ActionController::RoutingError, ActiveRecord::RecordNotFound,
+     ActionController::UnknownController, ::AbstractController::ActionNotFound]
+  end
+
+  def log_exception(ex)
+    Rails.logger.error "#{ex.class}: #{ex.message}\n  #{ex.backtrace.join("\n  ")}"
+  end
 
   def definir_host_do_mailer
     ActionMailer::Base.default_url_options[:host] = request.host_with_port
