d7e7b2c719047e679e7e2baca8d29ea147b83216
João Felipe <jfelipe.roque@gmail.com>
2013-01-24 14:35:06 -0200

    consertado teste 'Estatistica deve retornar usários cadastrados por ano' closes #376

--- spec/models/estatistica_spec.rb
+++ spec/models/estatistica_spec.rb
@@ -3,25 +3,25 @@
 require 'spec_helper'
 
 describe Estatistica do
-#  it 'deve retornar usuários cadastrados por ano' do
-#    ano = 2012
-#    Timecop.travel(2.years.ago) do
-#      3.times { create(:usuario) }
-#    end
-#    Timecop.travel(1.year.ago) do
-#      4.times { create(:usuario) }
-#    end
-#    2.times { create(:usuario) }
+  it 'deve retornar usuários cadastrados por ano' do
+    ano_atual = Date.today.year
+    Timecop.travel(2.years.ago) do
+      3.times { create(:usuario) }
+    end
+    Timecop.travel(1.year.ago) do
+      4.times { create(:usuario) }
+    end
+    2.times { create(:usuario) }
 
-#    estatisticas =  Estatistica.new(ano)
-#    estatisticas.numero_de_usuarios_cadastrados.should == 2
+    estatisticas =  Estatistica.new(ano_atual)
+    estatisticas.numero_de_usuarios_cadastrados.should == 2
 
-#    estatisticas = Estatistica.new(ano - 2)
-#    estatisticas.numero_de_usuarios_cadastrados.should == 3
+    estatisticas = Estatistica.new(ano_atual - 2)
+    estatisticas.numero_de_usuarios_cadastrados.should == 3
 
-#    estatisticas = Estatistica.new(ano - 1)
-#    estatisticas.numero_de_usuarios_cadastrados.should == 4
-#  end
+    estatisticas = Estatistica.new(ano_atual - 1)
+    estatisticas.numero_de_usuarios_cadastrados.should == 4
+  end
 
   it 'deve retornar usuários cadastrados por mês' do
     ano = 2012
