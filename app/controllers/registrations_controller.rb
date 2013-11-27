95885c66a2992419fb66e379761dc503f458f8df
Rodrigo Manhães <rmanhaes@gmail.com>
2013-11-27 01:51:35 -0200

    rails 4: campos adicionais para criacao de usuario (#388)

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,6 @@
 #encoding: utf-8
-class RegistrationsController < Devise::RegistrationsController
 
+class RegistrationsController < Devise::RegistrationsController
   def edit
     @user = current_user
   end
@@ -13,5 +13,13 @@ class RegistrationsController < Devise::RegistrationsController
        flash.now[:notice] = 'Suas alterações foram salvas com sucesso'
      end
     render :edit
+  end
+
+  private
+
+  def sign_up_params
+    devise_parameter_sanitizer.for(:sign_up).merge \
+      nome_completo: params[:usuario][:nome_completo],
+      campus_id: params[:usuario][:campus_id]
   end
 end
